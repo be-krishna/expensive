8545a7a074a19e0504ac36ce248f88547db7d400
balKrishna <bkshgnj@gmail.com>
2021-02-17 19:29:15 +0530

    added bottom sheet

--- lib/screens/recent_expenses.dart
+++ lib/screens/recent_expenses.dart
@@ -11,36 +11,60 @@ class RecentExpense extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            //* Add Sort Functionality
-            Expanded(
-              child: Consumer<ExpenseData>(
-                builder: (context, data, widget) => ListView.builder(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    itemCount: data.expenses.length,
-                    itemBuilder: (context, index) {
-                      var temp = data.sortedExpenses();
-                      var entry = temp[index];
-                      return ListItem(
-                        amount: entry.amount,
-                        note: entry.note,
-                        date: entry.date,
-                        category: entry.category,
-                        time: entry.time,
-                      );
-                    }),
-              ),
+      body: Column(
+        children: [
+          //* Add Sort Functionality
+          Expanded(
+            child: Consumer<ExpenseData>(
+              builder: (context, data, widget) => ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  itemCount: data.expensesOfMonth().length,
+                  itemBuilder: (context, index) {
+                    var temp = data.sortedExpenses();
+                    var entry = temp[index];
+                    return ListItem(
+                      amount: entry.amount,
+                      note: entry.note,
+                      date: entry.date,
+                      category: entry.category,
+                      time: entry.time,
+                    );
+                  }),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.pink,
         icon: Icon(Icons.sort),
         label: Text("Sort"),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            backgroundColor: Color(0xff0a0e21),
+            context: context,
+            builder: (context) {
+              return Wrap(
+                children: [
+                  ListTile(
+                    title: Text('All'),
+                  ),
+                  ListTile(
+                    title: Text('This Year'),
+                  ),
+                  ListTile(
+                    title: Text('This Month'),
+                  ),
+                  ListTile(
+                    title: Text('This Week'),
+                  ),
+                  ListTile(
+                    title: Text('Today'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
