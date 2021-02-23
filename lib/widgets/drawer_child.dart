e9b6f763593d75c9e9208a755249eb9b66d8b769
balKrishna <bkshgnj@gmail.com>
2021-02-23 22:10:39 +0530

    added fonts and drawer children

--- lib/widgets/drawer_child.dart
+++ lib/widgets/drawer_child.dart
@@ -5,30 +5,69 @@ import 'package:flutter/material.dart';
 class DrawerChild extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          child: Text('Drawer Header'),
-          decoration: BoxDecoration(
-            color: Colors.blue,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Column(
+        children: [
+          Flexible(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          "assets/images/icon.png",
+                          scale: 6,
+                        ),
+                        Text(
+                          "expensive",
+                          style: TextStyle(fontFamily: "Satisfy", fontSize: 32),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  title: Text(
+                    'Import',
+                    style: TextStyle(fontFamily: "OpenSans", fontSize: 16),
+                  ),
+                  trailing: Icon(Icons.file_download),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+                ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  title: Text(
+                    'Export',
+                    style: TextStyle(fontFamily: "OpenSans", fontSize: 16),
+                  ),
+                  trailing: Icon(Icons.file_upload),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-        ListTile(
-          title: Text('Item 1'),
-          onTap: () {
-            // Update the state of the app.
-            // ...
-          },
-        ),
-        ListTile(
-          title: Text('Item 2'),
-          onTap: () {
-            // Update the state of the app.
-            // ...
-          },
-        ),
-      ],
+          Text(
+            "Made with love",
+            style: TextStyle(fontFamily: "Satisfy", fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
